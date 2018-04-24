6213f0ed45e3cb1d217e91f1a180693b958fe3e3
Kimberly Huynh and Russell Hofvendahl <russell@>
2018-04-24 10:38:01 -0700

    user authentication works

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  root :to => 'questions#index'
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/signout' => 'sessions#destroy'
+
+  resources :questions
+end
---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory(:task) do
+    name('Knit the sock monkey')
+    priority(1)
+  end
+end
